478eec0fb3c55de30dfae7507ec13685c62ed998
Raskebay <zokoriya@mail.ru>
2022-04-18 16:11:08 +0300

    skip, params and markers

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,5 +1,4 @@
-
-
+import pytest
 from src.schemas.post import POST_SCHEMA
 
 from src.baseclasses.response import Response
@@ -10,5 +9,17 @@ def test_getting_post(get_posts, make_number):
     print(make_number)
 
 
+@pytest.mark.production
+@pytest.mark.skip
 def test_another():
     assert 1 == 1
+
+
+@pytest.mark.development
+@pytest.mark.parametrize('first_value, second_value, result', [(1, 2, 3),
+                                                               (-1, 2, 1),
+                                                               (-1, -2, -3),
+                                                               ('a', 2, None),
+                                                               ('a', 'b', None)])
+def test_calculator(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result
